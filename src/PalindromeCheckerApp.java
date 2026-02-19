9e776a2908a86cf4e51ffb91b060ff0b729d3161
shou-1202 <shouryagarg1202@gmail.com>
2026-02-19 09:24:03 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp{
     public static void main(String[] args){
         System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("@Shourya developer");
-        System.out.println("@version 1.0");
-
+        String s1;
+        Scanner input = new Scanner(System.in);
+        s1 = input.nextLine();
+        int i = 0, j = s1.length() - 1;
+        int flag = 1;
+        while(i<j){
+            if(s1.charAt(i) != s1.charAt(j)) {
+                flag = 0;
+                break;
+            }
+            i++;
+            j--;
+        }
+        if(flag==0){
+            System.out.println("It is not a palindrome");
+        }
+        else{
+            System.out.println("It is a palindrome");
+        }
     }
 }
